638238abcb1c3bf6cdc21ab7e46d07fa76ac2443
Naperehvate <tropikano647@mail.ru>
2024-04-28 23:51:25 +0500

    #что то добавили

---
+++ src/Main/components/Banner.jsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+    return (
+        <div className="banner-block">
+            <h2 className="fw-bold text-light">Change your mind today!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias voluptatum cupiditate facere?</p>
+        </div>
+    )
+}
+ 
+export default Banner;
